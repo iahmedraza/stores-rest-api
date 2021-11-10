b9565ffb97af1a3d6d09c0cc58f5e09f560d6f23
FarhanWBaig <farhanwbaig93@gmail.com>
2021-11-10 15:54:06 +0500

    RestApi access to Stores, Items and User Authentication

---
+++ resources/user.py
@@ -0,0 +1,32 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from models.user import UserModel
+
+
+
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("username",
+                type=str,
+                required = True,
+                help = "This field cannot be left blank!"
+            ) 
+    parser.add_argument("password",
+                type=str,
+                required = True,
+                help = "This field cannot be left blank!"
+            ) 
+        
+    def post(self):
+        
+        data =  UserRegister.parser.parse_args()
+
+        if UserModel.find_by_username(data['username']):
+            return {"message": "Username already exists"}, 400
+
+        new_user = UserModel(data["username"], data["password"])
+        new_user.add_new_user()
+
+        return {"message": "User created sucessfully."}, 201
+
